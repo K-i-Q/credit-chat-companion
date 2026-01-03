56b5be70b06ce8193a7bc11d00fc587063001097
Carlos Oliveira <carlos.oli1505@gmail.com>
2026-01-03 01:42:25 -0300

    analitics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider";
@@ -6,5 +7,6 @@ import { ThemeProvider } from "./components/theme-provider";
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
     <App />
+    <Analytics />
   </ThemeProvider>
 );
